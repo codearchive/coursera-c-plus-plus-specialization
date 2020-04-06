3602a5c01b60e393927c2eba8f20bb8351fcf4f4
Pavel Patrusov <pavelp9415@gmail.com>
2020-04-07 00:55:41 +0300

    Add solutions of "Blocks mass" and "Regional Database" tasks in the 1st week

---
+++ c-plus-plus-yellow-belt/week_1/block_mass/block_mass/block_mass.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned blocks_number;
+    unsigned short density;
+    cin >> blocks_number >> density;
+    uint64_t sum_mass = 0;
+    for (unsigned i = 0; i < blocks_number; ++i)
+    {
+        unsigned short width, height, depth;
+        cin >> width >> height >> depth;
+        sum_mass += static_cast<uint64_t>(width) * height * depth * density;
+    }
+    cout << sum_mass << endl;
+    return 0;
+}
