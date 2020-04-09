47669e2fd5c886e80463d7474c9f974aeb12c94d
Pavel Patrusov <pavelp9415@gmail.com>
2020-04-09 17:18:19 +0300

    Add solutions of "Element Reference" task in the 1st week

---
+++ c-plus-plus-yellow-belt/week_1/element_reference/element_reference/element_reference.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <map>
+#include <exception>
+#include <string>
+
+using namespace std;
+
+template <typename Key, typename Value> Value& GetRefStrict(map<Key, Value>& m, const Key& k);
+
+template <typename Key, typename Value>
+Value& GetRefStrict(map<Key, Value>& m, const Key& k)
+{
+    if (m.count(k) == 0)
+    {
+        throw runtime_error("Element with this key does not exist");
+    }
+    return m.at(k);
+}
+
+int main()
+{
+    map<int, string> m = { {0, "value"} };
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl;
+    return 0;
+}
