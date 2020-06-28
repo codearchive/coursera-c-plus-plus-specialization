845e9daefb7ca9362f00c364431b507168c03f80
Pavel <pavelp9415@gmail.com>
2020-06-28 20:56:49 +0300

    Add solution to the "Macros UPDATE_FIELD" task in the 1st week (red belt)

---
+++ c-plus-plus-red-belt/week_1/macros/avia_scanner_1/airline_ticket.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <iomanip>
+#include <ostream>
+#include <istream>
+
+using namespace std;
+
+struct Date {
+    int year, month, day;
+};
+
+struct Time {
+    int hours, minutes;
+};
+
+struct AirlineTicket {
+    string from;
+    string to;
+    string airline;
+    Date departure_date;
+    Time departure_time;
+    Date arrival_date;
+    Time arrival_time;
+    int price;
+
+
+};
+
+bool operator<(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) <
+            std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator==(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) ==
+            std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+std::ostream& operator<<(std::ostream& stream, const Date& date) {
+    stream << std::setw(4) << std::setfill('0') << date.year << '-'
+           << std::setw(2) << std::setfill('0') << date.month << '-'
+           << std::setw(2) << std::setfill('0') << date.day;
+    return stream;
+}
+
+std::istream& operator>>(std::istream& stream, Date& date) {
+    stream >> date.year;
+    stream.ignore(1);
+    stream >> date.month;
+    stream.ignore(1);
+    stream >> date.day;
+    return stream;
+}
+
+bool operator<(const Time& lhs, const Time& rhs) {
+    return std::tie(lhs.hours, lhs.minutes) <
+            std::tie(rhs.hours, rhs.minutes);
+}
+
+bool operator==(const Time& lhs, const Time& rhs) {
+    return std::tie(lhs.hours, lhs.minutes) ==
+            std::tie(rhs.hours, rhs.minutes);
+}
+
+std::ostream& operator<<(std::ostream& stream, const Time& time) {
+    stream << std::setw(2) << std::setfill('0') << time.hours << ':'
+           << std::setw(2) << std::setfill('0') << time.minutes;
+    return stream;
+}
+
+std::istream& operator>>(std::istream& stream, Time& time) {
+    stream >> time.hours;
+    stream.ignore(1);
+    stream >> time.minutes;
+    return stream;
+}
