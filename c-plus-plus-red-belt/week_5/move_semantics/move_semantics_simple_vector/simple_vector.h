dd11393eeab36922f46cc78e780dd8125989304a
Pavel <pavelp9415@gmail.com>
2020-09-20 23:16:49 +0300

    Add solution to the "Move-semantics in SimpleVector" task in the 5th week (red belt)

---
+++ c-plus-plus-red-belt/week_5/move_semantics/move_semantics_simple_vector/simple_vector.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdlib>
+
+template <typename T>
+class SimpleVector {
+public:
+    SimpleVector() = default;
+    explicit SimpleVector(size_t size);
+    ~SimpleVector();
+
+    T& operator[](size_t index);
+
+    const T* begin() const;
+    const T* end() const;
+    T* begin();
+    T* end();
+
+    size_t Size() const;
+    size_t Capacity() const;
+    void PushBack(const T& value);
+    void PushBack(T&& value);
+private:
+    T* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
+
+template<typename T>
+SimpleVector<T>::SimpleVector(size_t size)
+    : data_(new T[size]), size_(size), capacity_(size)
+{}
+
+template<typename T>
+SimpleVector<T>::~SimpleVector() {
+    delete[] data_;
+}
+
+template<typename T>
+T& SimpleVector<T>::operator[](size_t index) {
+    return data_[index];
+}
+
+template<typename T>
+T* SimpleVector<T>::begin() {
+    return data_;
+}
+
+template<typename T>
+T* SimpleVector<T>::end() {
+    return data_ + size_;
+}
+
+template<typename T>
+const T* SimpleVector<T>::begin() const {
+    return data_;
+}
+
+template<typename T>
+const T* SimpleVector<T>::end() const {
+    return data_ + size_;
+}
+
+template<typename T>
+size_t SimpleVector<T>::Size() const {
+    return size_;
+}
+
+template<typename T>
+size_t SimpleVector<T>::Capacity() const {
+    return capacity_;
+}
+
+template<typename T>
+void SimpleVector<T>::PushBack(const T& value) {
+    if (capacity_ == 0) {
+        capacity_ = 1;
+        data_ = new T[capacity_];
+    }
+    else if (size_ >= capacity_) {
+        capacity_ *= 2;
+        T* new_data = new T[capacity_];
+        std::move(data_, data_ + size_, new_data);
+        delete[] data_;
+        data_ = new_data;
+    }
+    data_[size_++] = value;
+}
+
+template<typename T>
+void SimpleVector<T>::PushBack(T&& value) {
+    if (capacity_ == 0) {
+        capacity_ = 1;
+        data_ = new T[capacity_];
+    }
+    else if (size_ >= capacity_) {
+        capacity_ *= 2;
+        T* new_data = new T[capacity_];
+        std::move(data_, data_ + size_, new_data);
+        delete[] data_;
+        data_ = new_data;
+    }
+    data_[size_++] = std::move(value);
+}
